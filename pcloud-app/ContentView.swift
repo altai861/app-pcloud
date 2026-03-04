6164d4fad22f4abb265bdae64b69e0dc9138c7fb
altai.g <bbyyydriver@gmail.com>
2026-03-04 13:30:14 +0800

    Initial Commit

---
+++ pcloud-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pcloud-app
+//
+//  Created by Altai Gantumur on 2026.03.04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pcloud-app/pcloud_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pcloud_appApp.swift
+//  pcloud-app
+//
+//  Created by Altai Gantumur on 2026.03.04.
+//
+
+import SwiftUI
+
+@main
+struct pcloud_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
