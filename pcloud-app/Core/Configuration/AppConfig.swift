0c8109dcf453f55ffcf233506a0812ce6fbc563f
altai.g <bbyyydriver@gmail.com>
2026-05-17 10:36:26 +0800

    relay server ee url tohiruulav

--- pcloud-app/Core/Configuration/AppConfig.swift
+++ pcloud-app/Core/Configuration/AppConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum AppConfig {
     static let defaultAPIBaseURLString = "http://192.168.0.105:8080"
-    static let defaultRelayBaseURLString = "http://192.168.0.105:7070"
+    static let defaultRelayBaseURLString = "https://relay.uneliy.org"
     static let accessTokenService = "com.cloud.pcloud-app.access-token"
     static let accessTokenAccount = "default-session"
 }
